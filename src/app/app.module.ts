2506ddaca9a16d07b487e8c090022248dd2d3e9d
kikeangeleska <hristinaangeleska@gmail.com>
2021-12-12 01:11:00 +0100

    Number to LCD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LcdDisplayComponent } from './lcd-display/lcd-display.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [CommonModule, BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, LcdDisplayComponent],
+  bootstrap: [LcdDisplayComponent],
 })
-export class AppModule { }
+export class AppModule {}
